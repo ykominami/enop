b5679763868e99da04aa0200ea710b18f02f6ec7
yasuo kominami <ykominami@gmail.com>
2022-03-29 07:08:11 +0900

    update dependency and retrive notes having pdf

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in enop.gemspec
 gemspec
-gem "arxutils", :path => 'c:/Users/ykomi/cur/ruby/arxutils-gem/arxutils'
-gem "simpleoptparse", :path => 'c:/Users/ykomi/cur/ruby/simpleoptparse-gem/simpleoptparse2'
+gem "arxutils", :path => 'c:/Users/ykomi/cur/ruby/arxutils'
+gem "simpleoptparse", :path => 'c:/Users/ykomi/cur/ruby/simpleoptparse'
+
+gem "standalone_migrations"
--- lib/enop/enop.rb
+++ lib/enop/enop.rb
@@ -1,56 +1,71 @@
 # -*- coding: utf-8 -*-
-require 'evernote-thrift'
-require 'csv'
-require 'pp'
-require 'openssl'
-require 'forwardable'
-
+require "evernote-thrift"
+require "csv"
+require "pp"
+require "openssl"
+require "forwardable"
+require "pstore"
+require "json"
+#require "enop_sub"
 module Enop
   # Evernote操作クラス
   class Enop
     # 初期化
-    def initialize( authToken , hs , opts, userStoreUrl = nil )
+    attr_reader :notebooks_hs, :notebooks_hs_backup, :notebooks_hs_notelist_backup
+
+    def initialize(authToken, hs, opts, userStoreUrl = nil)
       # SSL認証を行わないように変更
-      OpenSSL::SSL.module_eval{ remove_const(:VERIFY_PEER) }
-      OpenSSL::SSL.const_set( :VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE )
+      OpenSSL::SSL.module_eval { remove_const(:VERIFY_PEER) }
+      OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+      @store_db = PStore.new(Pathname.new(hs["output_dir"]) + "enop.dump")
+      @notebooks_hs = {}
+      @notebooks = []
+      @store_db.transaction do
+        @store_db.delete(:notebooks)
+        @notebooks_hs_notelist_backup = @store_db.fetch(:notebooks_hs_notelist, {})
+      end
+      #hs = { notebooks_hs: @notebooks_hs, notelist: notelist }
+
+      @notebooks_hs = @notebooks_hs_notelist_backup[:notebooks_hs]
+      @notelist = @notebooks_hs_notelist_backup[:notelist]
+      @memox = @notebooks_hs_notelist_backup[:memox]
 
       # Evernoteのノートブックスタックの配列
       @stack_hs = {}
       # ノートブック情報の配列
       @nbinfos = {}
       # ノートブック情報のStruct
-      @notebookinfo = Struct.new("NotebookInfo", :name, :stack, :defaultNotebook, :count , :tags )
+      @notebookinfo = Struct.new("NotebookInfo", :name, :stack, :defaultNotebook, :count, :tags)
 
       # 認証トークン
       @authToken = authToken
 
-      register_time = Arxutils::Dbutil::DbMgr.init( hs["db_dir"], hs["migrate_dir"] , hs["config_dir"], hs["dbconfig"] , hs["env"] , hs["log_fname"] , opts )
+      register_time = Arxutils::Dbutil::DbMgr.init(hs["db_dir"], hs["migrate_dir"], hs["config_dir"], hs["dbconfig"], hs["env"], hs["log_fname"], opts)
 
       # 保存用DBマネージャ
-      @dbmgr = ::Enop::Dbutil::EnopMgr.new( register_time )
+      @dbmgr = ::Enop::Dbutil::EnopMgr.new(register_time)
       puts "@dbmgr=#{@dbmgr}"
 
-
       evernoteHost = "www.evernote.com"
-      userStoreUrl = "https://#{evernoteHost}/shard/s18/notestore"
+      userStoreUrl = "https://#{evernoteHost}/shard/s18/notestore" unless userStoreUrl
       userStoreTransport = Thrift::HTTPClientTransport.new(userStoreUrl)
       userStoreProtocol = Thrift::BinaryProtocol.new(userStoreTransport)
       # Evernoteユーザストア
       @userStore = Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)
       # Invalid method name : 'checkVersion' が返されるので、とりあえずコメント化
-      set_output_dest( hs["output_dir"] , get_output_filename_base )
+      set_output_dest(hs["output_dir"], get_output_filename_base)
     end
 
     # 出力先設定
-    def set_output_dest( parent_dir , fname_base )
+    def set_output_dest(parent_dir, fname_base)
       if fname_base
-		    outfname = File.join( parent_dir , fname_base )
+        outfname = File.join(parent_dir, fname_base)
         outfname_txt = outfname + ".txt"
         outfname_csv = outfname + ".csv"
         # 出力先ファイル
-        @output = File.open( outfname_txt , "w" , { :encoding => 'UTF-8' } )
+        @output = File.open(outfname_txt, "w", { :encoding => "UTF-8" })
         # 出力先ファイル(CSV形式)
-        @output_csv = CSV.open( outfname_csv , "w" , { :encoding => 'UTF-8' } )
+        @output_csv = CSV.open(outfname_csv, "w", { :encoding => "UTF-8" })
       else
         @output = STDOUT
       end
@@ -62,8 +77,8 @@ module Enop
     end
 
     # 出力先に文字列出力
-    def putsx( str )
-      @output.puts( str )
+    def putsx(str)
+      @output.puts(str)
     end
 
     # Evernoteへ接続
@@ -81,9 +96,12 @@ module Enop
     end
 
     # Evernoteノートブック取得
-    def get_notebooks
+    def get_notebooks_from_remote
+
+      notebooks_hs = {}
       begin
         notebooks = @noteStore.listNotebooks(@authToken)
+        notebooks_hs = Hash[*notebooks.map { |notebook| [notebook.guid, notebook] }.flatten]
       rescue Evernote::EDAM::Error::EDAMUserException => ex
         parameter = ex.parameter
         errorCode = ex.errorCode
@@ -96,21 +114,21 @@ module Enop
         puts "@authToken=#{@authToken}"
         puts "Can't call listNotebooks"
         p ex
-        exit
+        exit(2)
       end
 
-      puts "Found #{notebooks.size} notebooks:"
+      #puts "Found #{notebooks.size} notebooks:"
 
-      notebooks
+      notebooks_hs
     end
 
     # 指定Evernoteノートブックが含むノート数取得
-    def get_note_count( notebookguid , filter = nil )
+    def get_note_count(notebookguid, filter = nil)
       filter = Evernote::EDAM::NoteStore::NoteFilter.new unless filter
       filter.notebookGuid = notebookguid
       ret = nil
       begin
-        ret = @noteStore.findNoteCounts(@authToken , filter , false )
+        ret = @noteStore.findNoteCounts(@authToken, filter, false)
       rescue => ex
         puts "Can't call findNoteCounts with #{notebookguid}"
       end
@@ -119,15 +137,15 @@ module Enop
     end
 
     # 文字列で指定したノートブックに含まれるノートのタイトルとソースのURLの配列取得
-    def get_url_from_notebook( name )
+    def get_url_from_notebook(name)
       # Evernoteノートブック配列
-      @notebooks = get_notebooks unless @notebooks
+      get_notebooks
 
       filter = Evernote::EDAM::NoteStore::NoteFilter.new
 
-      notebook = @notebooks.find{ |x| x.name == name }
+      notebook = @notebooks_hs.values.find { |x| x.name == name }
       filter.notebookGuid = notebook.guid
-      ret = get_note_count( notebook.guid , filter )
+      ret = get_note_count(notebook.guid, filter)
       notebookCounts = 0
       if ret
         if ret.notebookCounts
@@ -141,67 +159,336 @@ module Enop
       array_of_array = []
       count_unit = 100
       count = 0
-      count.step(notebookCounts , count_unit) {|cnt|
+      count.step(notebookCounts, count_unit) { |cnt|
         ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, cnt, count_unit, spec)
-        array_of_array << ourNoteList.notes.map{ |x| [x.title , x.attributes.sourceURL] }
+        array_of_array << ourNoteList.notes.map { |x| [x.title, x.attributes.sourceURL] }
       }
       array_of_array.flatten(1)
     end
 
-    # Evernoteノートブックに関する情報一覧
-    def list_notebooks
-      # List all of the notebooks in the user's account
+    def get_notebooks_hs
+      if @notebooks_hs.size == 0
+        @notebooks_hs = get_notebooks_from_remote
+        # Evernoteノートブック配列
+        @notebooks_hs_notelist_backup[:notebooks_hs] = @notebooks_hs
 
-      # Evernoteノートブック配列
-      @notebooks = get_notebooks unless @notebooks
-
-      filter = Evernote::EDAM::NoteStore::NoteFilter.new
-
-      memo = @notebooks.inject({:defaultNotebook => nil , :nbinfo => []}) do |memo , notebook|
-        notebook_name = ( notebook.name == nil ? "" : notebook.name )
-        stack_name = ( notebook.stack == nil ? "" : notebook.stack )
-        ret = get_note_count( notebook.guid , filter )
-        if ret
-          if ret.notebookCounts
-            notebookCounts = ret.notebookCounts[notebook.guid]
-          else
-            notebookCounts = 0
-          end
-          tagcount = 0
-          if ret.tagCounts
-            tagcount = ret.tagCounts.size
-          end
-          nbinfo = @notebookinfo.new( notebook_name , stack_name , notebook.defaultNotebook , notebookCounts , tagcount )
-          # CSVファイルへ追加（自動的に出力）
-          register_notebook( nbinfo.stack , nbinfo )
-          # dbへの登録
-          count = nbinfo.count
-          count ||= 0
-          @dbmgr.add(  nbinfo.stack , nbinfo.name , count , nbinfo.tags.size )
-
-          #  p notebook
-          memo[:defaultNotebook] = nbinfo if nbinfo.defaultNotebook
-          memo[:nbinfo] << nbinfo
+        @store_db.transaction do
+          @store_db[:notebooks_hs_notelist] = @notebooks_hs_notelist_backup
         end
-
-        memo
       end
+      @notebooks_hs
+    end
 
-      @stack_hs.keys.sort.each do |k|
-        # TXTファイルに出力
-        putsx "#{k},#{@stack_hs[k]}"
+    def get_notebooks_hs_from_backup
+      p "get_notebooks_hs_from_backup"
+      if @notebooks_hs.size == 0
+        p "get_notebooks_hs_from_backup 1"
+
+        get_notebooks_hs
       end
+      @notebooks_hs
+    end
 
-      pp memo[:defaultNotebook]
+    def get_all_notebooks_hs(from_backup = false)
+      from_backup ? get_notebooks_hs_from_backup : get_notebooks_hs
+    end
+
+    def output_in_json(obj)
+      putsx(JSON.pretty_generate(obj))
     end
 
     # スタックにノートブックに関する情報を登録
-    def register_notebook( stack , nbinfo )
+    def register_notebook(stack, nbinfo)
       @stack_hs[stack] ||= []
       @stack_hs[stack] << nbinfo
       @nbinfos[nbinfo.name] = nbinfo
 
-      @output_csv << [ stack , nbinfo.name , nbinfo.count ]
+      @output_csv << [stack, nbinfo.name, nbinfo.count]
+    end
+
+    def get_notes_having_pdf_sub_0(filter, spec, head, unit, total = nil)
+      p "get_notes_having_pdf_sub 1"
+      ary = []
+
+      tail = head + unit - 1
+      tail = total if total
+
+      head.step(tail, unit) { |i|
+        #puts "i=#{i}"
+        limit = i + unit - 1
+        limit = tail if limit >= tail
+        puts "i=#{i} limit=#{limit}"
+        ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, i, limit, spec)
+        #ourNoteList = OpenStruct.new
+        #ourNoteList.totalNotes = 1000
+        #ourNoteList.notes = [ {}, {} ]
+        puts "ourNoteList.totalNotes=#{ourNoteList.totalNotes}"
+        puts "ourNoteList.notes.size=#{ourNoteList.notes.size}"
+        ary << ourNoteList
+      }
+      ary
+    end
+
+    def get_notes_having_pdf_sub(filter, spec, head, unit, total = nil)
+      p "get_notes_having_pdf_sub 1"
+      ary = []
+
+      tail = head + unit - 1
+      tail = total if total
+
+      head.step(tail, unit) { |i|
+        #puts "i=#{i}"
+        limit = i + unit - 1
+        limit = tail if limit >= tail
+        puts "i=#{i} limit=#{limit}"
+        ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, i, limit, spec)
+        #ourNoteList = OpenStruct.new
+        #ourNoteList.totalNotes = 1000
+        #ourNoteList.notes = [ {}, {} ]
+        puts "ourNoteList.totalNotes=#{ourNoteList.totalNotes}"
+        puts "ourNoteList.notes.size=#{ourNoteList.notes.size}"
+        ary << ourNoteList
+      }
+      ary
+    end
+
+    def get_notes_having_pdf_from_remote
+      puts " get_notes_having_pdf_from_remote 0"
+      filter = Evernote::EDAM::NoteStore::NoteFilter.new
+      filter.ascending = false
+      filter.words = "resource:application/pdf"
+
+      spec = Evernote::EDAM::NoteStore::NotesMetadataResultSpec.new
+      spec.includeTitle = true
+      spec.includeCreated = true
+      spec.includeUpdated = true
+      spec.includeDeleted = true
+      spec.includeUpdateSequenceNum = true
+      spec.includeNotebookGuid = true
+      spec.includeTagGuids = true
+      spec.includeAttributes = true
+      #spec.includeNotebookGuid = true
+
+      head = 0
+      unit = 500
+      tail = head + unit
+      notelists = []
+      # ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, head, tail, spec)
+      # ourNoteList = get_notes_having_pdf_sub(filter, spec, head, unit)
+      ary = get_notes_having_pdf_sub(filter, spec, head, unit)
+      next_head = head + unit
+      # total = ourNoteList.totalNotes
+      total = ary[0].totalNotes
+      p "total=#{total}"
+      notelists << ary[0].notes
+      # ourNoteList = get_notes_having_pdf_sub(filter, spec, next_head, unit, total)
+      ary = get_notes_having_pdf_sub(filter, spec, next_head, unit, total)
+      notelists << ary.map { |x| x.notes }
+      notelist = notelists.flatten
+      p notelist.size
+
+      hs = { notebooks_hs: @notebooks_hs, notelist: notelist }
+      output_in_json(hs)
+      @store_db.transaction do
+        @store_db[:notebooks_hs_notelist] = hs
+      end
+      @notebooks_hs_notelist_backup = hs
+    end
+
+    def get_notes_having_pdf(from_backup = false)
+      get_all_notebooks_hs(from_backup)
+
+      get_notes_having_pdf_from_remote unless from_backup
+
+      @notebooks_hs_notelist_backup[:notelist]
+    end
+
+    def make_filter(words)
+      filter = Evernote::EDAM::NoteStore::NoteFilter.new
+      filter.ascending = false
+      filter.words = words
+      filter
+    end
+
+    def list_note_having_pdf(from_backup = false)
+      get_notes_having_pdf(from_backup)
+      filter = make_filter("resource:application/pdf")
+      pp "@notelist.size=#{@notelist.size}"
+      pp "===="
+      stacks = @notelist.reduce({}) { |stack, note|
+        item = OpenStruct.new
+        item.guid = note.guid
+        item.title = note.title
+        item.notebook_guid = note.notebookGuid
+        item.notebook_name = @notebooks_hs[note.notebookGuid].name
+        item.stack = @notebooks_hs[note.notebookGuid].stack
+        item.stack = "" unless item.stack
+        #pp @notebooks_hs[note.notebookGuid]
+        #exit
+        stack[item.stack] ||= {}
+        stack[item.stack][item.notebook_name] ||= []
+        stack[item.stack][item.notebook_name] << item
+        stack
+      }
+      stacks.keys.sort.map { |name|
+        puts name
+        stacks[name].keys.sort.map { |x|
+           puts " #{x}"
+           puts stacks[name][x].map { |note|
+             "  #{note.title} #{note.guid}"
+           }
+        }
+      }
+    end
+
+
+    def list_note_having_pdf_0(from_backup = false)
+      get_notes_having_pdf(from_backup)
+      filter = make_filter("resource:application/pdf")
+      pp "@notelist.size=#{@notelist.size}"
+      pp "===="
+      stacks = @notelist.reduce({}) { |stack, note|
+        item = OpenStruct.new
+        item.guid = note.guid
+        item.title = note.title
+        item.notebook_guid = note.notebookGuid
+        item.notebook_name = @notebooks_hs[note.notebookGuid].name
+        item.stack = @notebooks_hs[note.notebookGuid].stack
+        item.stack = "" unless item.stack
+        #pp @notebooks_hs[note.notebookGuid]
+        #exit
+        stack[item.stack] ||= {}
+        stack[item.stack][item.notebook_name] ||= []
+        stack[item.stack][item.notebook_name] << item
+        stack
+      }
+      stacks.keys.sort.map { |name|
+        puts name
+        stacks[name].keys.sort.map { |x|
+           puts " #{x}"
+           puts stacks[name][x].map { |note|
+             "  #{note.title} #{note.guid}"
+           }
+        }
+      }
+    end
+
+    def make_filter_with_notebook_guid(notebook_guid)
+      filter = make_filter("resource:application/pdf")
+      filter.notebookGuid = notebook_guid
+      filter
+    end
+
+    def get_stack_notebooks(from_backup)
+      notebooks_hs = get_all_notebooks_hs(from_backup)
+      memox = notebooks_hs.keys.reduce({}){ |memo, guid|
+        nb = notebooks_hs[guid]
+        stack = nb.stack
+        stack = "" unless stack
+        memo[stack] ||= {}
+        item = OpenStruct.new
+        item.name = nb.name
+        item.name = "" unless item.name
+        item.guid = nb.guid
+        memo[stack][nb.name] = item
+        memo
+      }
+    end
+
+    def list_notebooks(from_backup)
+      memox = get_stack_notebooks(from_backup)
+      memox.keys.sort.map { |slack|
+        puts slack
+        memox[slack].keys.sort.map { |nb_name|
+          item = memox[slack][nb_name]
+          puts " #{nb_name} #{item.guid}"
+        }
+      }
+    end
+
+    def get_note_having_pdf_by_notebook(guid, spec)
+      filter = make_filter_with_notebook_guid(guid)
+      ary = []
+      head = 0
+      unit = 100
+      notelists = []
+      i = head
+      ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, i, unit, spec)
+      ary << ourNoteList
+      totalNotes = ourNoteList.totalNotes
+      i = i + ourNoteList.notes.size
+      while i < totalNotes
+        puts "#{i}/#{totalNotes}"
+        ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, i, unit, spec)
+        ary <<  ourNoteList
+        break if ourNoteList.notes.size == 0 
+        i = i + ourNoteList.notes.size
+      end
+      notelist = ary.map{ |x| x.notes }.flatten
+      notelist.size
+    end
+
+    def make_spec
+      spec = Evernote::EDAM::NoteStore::NotesMetadataResultSpec.new
+      spec.includeTitle = true
+      spec.includeCreated = true
+      spec.includeUpdated = true
+      spec.includeDeleted = true
+      spec.includeUpdateSequenceNum = true
+      spec.includeNotebookGuid = true
+      spec.includeTagGuids = true
+      spec.includeAttributes = true
+      #spec.includeNotebookGuid = true
+      spec
+    end
+
+    def list_notebooks_having_pdf(from_backup)
+      spec = make_spec
+      memox = get_stack_notebooks(from_backup)
+      stack_list = []
+      stack_list << %W(8-sci)
+      stack_list << %W(1-dev-lang)
+      stack_list.flatten.map{ |stack|
+        memox[stack].keys.sort.map{ |x| 
+          guid = memox[stack][x].guid
+          puts "#{x} #{guid}"
+          size = get_note_having_pdf_by_notebook(guid, spec)
+          memox[stack][x].size_of_notes = size
+          pp size
+        }
+      }
+      @notebooks_hs_notelist_backup[:memox] = memox
+      @store_db.transaction do
+        @store_db[:notebooks_hs_notelist] = @notebooks_hs_notelist_backup
+      end
+    end
+
+    def getLatest100Notes(from_backup = false)
+      notebooks_hs = get_all_notebooks_hs(from_backup)
+
+      filter = Evernote::EDAM::NoteStore::NoteFilter.new
+      filter.ascending = false
+      filter.words = "resource:application/pdf"
+      spec = Evernote::EDAM::NoteStore::NotesMetadataResultSpec.new
+      spec.includeTitle = true
+      spec.includeCreated = true
+      spec.includeUpdated = true
+      spec.includeDeleted = true
+      spec.includeUpdateSequenceNum = true
+      spec.includeNotebookGuid = true
+      spec.includeTagGuids = true
+      spec.includeAttributes = true
+      #spec.includeNotebookGuid = true
+
+      head = 0
+      unit = 10
+      tail = head + unit
+      ourNoteList = @noteStore.findNotesMetadata(@authToken, filter, head, tail, spec)
+      hs = { notebooks_hs: notebooks_hs, notelist: ourNoteList }
+      output_in_json(hs)
+      @store_db.transaction do
+        @store_db[:notebooks_hs_notelist] = hs
+      end
     end
   end
 end
