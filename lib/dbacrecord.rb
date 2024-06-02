561a920948adb100a01b85aa182a5f42a3f5ffa4
ykominami <ykominami@gmail.com>
2024-06-03 00:20:41 +0900

    fix

--- lib/dbacrecord.rb
+++ lib/dbacrecord.rb
@@ -1,27 +1,26 @@
-# frozen_string_literal: true
-
 module Enop
-  module Dbutil
-    class Count < ActiveRecord::Base
-      has_many :invalidennblists
-    end
-
-    class Countdatetime < ActiveRecord::Base
-    end
-
-    class Evnb < ActiveRecord::Base
-    end
-
-    class Ennblist < ActiveRecord::Base
-    end
-
-    class Invalidennblist < ActiveRecord::Base
-      belongs_to :ennblist, foreign_key: 'org_id'
-      belongs_to :count, foreign_key: ''
-    end
-
-    class Currentennblist < ActiveRecord::Base
-      belongs_to :ennblist, foreign_key: 'org_id'
-    end
+module Dbutil
+  class Count < ActiveRecord::Base
+    has_many :invalidennblists
   end
+
+  class Countdatetime < ActiveRecord::Base
+  end
+
+  class Evnb < ActiveRecord::Base
+  end
+
+  class Ennblist < ActiveRecord::Base
+  end
+
+  class Invalidennblist < ActiveRecord::Base
+    belongs_to :ennblist, foreign_key: 'org_id'
+    belongs_to :count, foreign_key: ''
+  end
+
+  class Currentennblist < ActiveRecord::Base
+    belongs_to :ennblist, foreign_key: 'org_id'
+  end
+
+end
 end
